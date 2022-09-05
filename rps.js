06798f23c4daa5d8cefce59f442e849666d19c86
Christian Tarello <christian_tarello@outlook.com>
2022-09-04 21:30:45 -0500

    Add code to playerRound

--- rps.js
+++ rps.js
@@ -22,13 +22,28 @@ function getComputerChoice(){
 }
 
 function playRound(PlayerSelection, ComputerSelection){
-    /*Standarize PlayerSelection using a helper function and store the result in the same variable
+    /*Standarize PlayerSelection using a helper function and store the result in the same variable*/
+    PlayerSelection = capitalizeFirstLetter(PlayerSelection);
     /*Format win string and store it in a variable*/
+    let winString = `You win! {PlayerSelection} beats {ComputerSelection}.`
     /*Format lose string and store it in a variable*/
+    let loseString = `You lose! {ComputerSelection} beats {PlayerSelection}.`
     /*Format tie string and store it in a variable*/
-    /*Create tie condition and return the tie string if true and move on to the losing conditions if false
-    /*Create the losing conditions for r/p/s vs p/s/r and return the losing string if true
+    let tieString = `It's a tie.`
+    /*Create tie condition and return the tie string if true and move on to the losing conditions if false*/
+    /*Create the losing conditions for r/p/s vs p/s/r and return the losing string if true*/
     /*If none of the conditions apply return win string*/
+    if (PlayerSelection === ComputerSelection){
+        return tieString
+    } else if (PlayerSelection === "Rock" && ComputerSelection === "Paper"){
+        return loseString
+    } else if (PlayerSelection === "Paper" && ComputerSelection === "Scissors"){
+        return loseString
+    } else if (PlayerSelection === "Scissors" && ComputerSelection === "Rock"){
+        return loseString
+    } else {
+        return winString
+    }
 }
 
 function game(){
